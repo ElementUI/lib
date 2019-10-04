e9a71b572431b33ea6451311e4f517fb731c9b0b
Cahya Bagus Sanjaya <cbsanjaya@users.noreply.github.com>
2019-10-04 15:20:24 +0700

    Update id.js

--- locale/lang/id.js
+++ locale/lang/id.js
@@ -19,10 +19,10 @@ exports.default = {
       startTime: 'Waktu Mulai',
       endDate: 'Tanggal Selesai',
       endTime: 'Waktu Selesai',
-      prevYear: 'Previous Year', // to be translated
-      nextYear: 'Next Year', // to be translated
-      prevMonth: 'Previous Month', // to be translated
-      nextMonth: 'Next Month', // to be translated
+      prevYear: 'Tahun Sebelumnya',
+      nextYear: 'Tahun Setelahnya',
+      prevMonth: 'Bulan Sebelumnya',
+      nextMonth: 'Bulan Setelahnya',
       year: 'Tahun',
       month1: 'Januari',
       month2: 'Februari',
@@ -86,7 +86,7 @@ exports.default = {
       error: 'Masukan ilegal'
     },
     upload: {
-      deleteTip: 'press delete to remove', // to be translated
+      deleteTip: 'Tekan hapus untuk membuang',
       delete: 'Hapus',
       preview: 'Pratinjau',
       continue: 'Lanjutkan'
@@ -110,10 +110,10 @@ exports.default = {
       hasCheckedFormat: '{checked}/{total} terpilih'
     },
     image: {
-      error: 'FAILED' // to be translated
+      error: 'GAGAL'
     },
     pageHeader: {
-      title: 'Back' // to be translated
+      title: 'Kembali'
     }
   }
 };
