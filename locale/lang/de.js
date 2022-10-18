b06d3ff0d2b21f7bd1bfdb0bbe0cfbf5057576a5
Dimitris Gerasopoulos <dimgera84@hotmail.com>
2022-10-18 12:00:49 +0200

    Update de.js

--- locale/lang/de.js
+++ locale/lang/de.js
@@ -111,14 +111,14 @@ exports.default = {
       hasCheckedFormat: '{checked}/{total} ausgewählt'
     },
     image: {
-      error: 'FAILED' // to be translated
+      error: 'Fehlgeschlagen' 
     },
     pageHeader: {
-      title: 'Back' // to be translated
+      title: 'Zurück' 
     },
     popconfirm: {
-      confirmButtonText: 'Yes', // to be translated
-      cancelButtonText: 'No' // to be translated
+      confirmButtonText: 'Ja', 
+      cancelButtonText: 'Nein'
     },
     empty: {
       description: 'Keine Daten'
